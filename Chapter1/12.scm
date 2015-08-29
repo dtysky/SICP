6b380d7df90194fa0680bb18f7d64a2158f7181b
dtysky <dtysky@outlook.com>
2015-08-30 00:30:36 +0800

    1-14 Finished.
    Signed-off-by: dtysky <dtysky@outlook.com>

--- Chapter1/12.scm
+++ Chapter1/12.scm
@@ -1,5 +1,5 @@
 ;x and y are all start from 1
-(define (pasic x y)
+(define (pascal x y)
   (cond ((= y 1) 1)
         ((or (= x 1)
               (= x y))
@@ -7,7 +7,7 @@
         ((or (< x 1)
             (> x y))
             0)
-        (else (+ (pasic x
+        (else (+ (pascal x
                         (- y 1))
-                 (pasic (- x 1)
+                 (pascal (- x 1)
                         (- y 1))))))
---
+++ Chapter1/13.scm
@@ -0,0 +1,1 @@
+;Skip now TAT.
