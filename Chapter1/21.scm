47ce9d8b1a5358414d145c1512575dc94cc14868
dtysky <dtysky@outlook.com>
2015-09-01 23:06:42 +0800

    1-21 Finished.
    Signed-off-by: dtysky <dtysky@outlook.com>

---
+++ Chapter1/21.scm
@@ -0,0 +1,22 @@
+(define (square x)
+  (* x x))
+
+(define (divides? a b)
+  (= (remainder b a ) 0))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(display (prime? 199))
+(display #\newline)
+(display (prime? 1999))
+(display #\newline)
+(display (prime? 19999))
