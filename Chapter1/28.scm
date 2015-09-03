bddecfcd739d9dacc4e57efbe03ee251369d4b3b
dtysky <dtysky@outlook.com>
2015-09-03 23:39:16 +0800

    1-28 Finished.
    Signed-off-by: dtysky <dtysky@outlook.com>

---
+++ Chapter1/28.scm
@@ -0,0 +1,43 @@
+#lang planet neil/sicp
+
+(define (square x)
+  (* x x))
+
+(define (trivial-square a n)
+  (if (= (remainder (square a) n) 1)
+      0
+      a))
+  
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (trivial-square (expmod base (/ exp 2) m) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (miller-rabin n)
+  (define (check a n)
+    (cond ((= a n) true)
+          ((= (expmod a
+                      (- n 1)
+                      n)
+              1)
+          (check (+ a 1) n))
+          (else false)))
+  (if (check 2 n)
+      "Yes"
+      "No"))
+
+(define (test x)
+  (display x)
+  (display " *** ")
+  (display (miller-rabin x))
+  (newline))
+
+(define test-num '(3 7 13 211 561 1105 1729 2465 2821 6601))
+
+(map (lambda (x) (test x))
+       test-num)
